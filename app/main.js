507ebc16da0740084ca42b36949ef5c853a7a777
OliverOliverOliverAnders <OliverAnders@olivers-imac-3.speedport_w_724v_09011603_00_018>
2016-03-14 07:19:40 +0100

    new

--- app/main.js
+++ app/main.js
@@ -1,7 +1,14 @@
 import React from 'react';
-import {render} from 'react-dom';
+import ReactDOM from 'react-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
 import App from './App';
 import './main.css';
 
+//Needed for onTouchTap
+//Can go away when react 1.0 release
+//Check this repo:
+//https://github.com/zilverline/react-tap-event-plugin
+injectTapEventPlugin();
 
-render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
