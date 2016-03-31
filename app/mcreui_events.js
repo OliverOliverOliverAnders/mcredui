cf9a4a95c0b4d128cf4d222ebd871b30e5511bca
OliverOliverOliverAnders <oliveranders@Olivers-MacBook-Pro.local>
2016-03-31 20:34:38 +0200

    fixed table

--- app/mcreui_events.js
+++ app/mcreui_events.js
@@ -1,5 +1,8 @@
 /* EventListPage */
 import React from 'react';
+import ReactDOM from 'react-dom';
+import {Table, Column, Cell} from 'fixed-data-table';
+
 var ReactDataGrid = require('react-data-grid');
 var ReactDataGrid = require('react-data-grid/addons');
 
@@ -12,42 +15,40 @@ const MCREUIEventList=React.createClass({
   };
   },
   render(){
-    var _rows = [];
-    for (var i = 1; i < 1000; i++) {
-      _rows.push({
-        id: i,
-        title: 'Title ' + i,
-        count: i * 1000
-      });
-    }
 
-    //A rowGetter function is required by the grid to retrieve a row for a given index
-    var rowGetter = function(i){
-      return _rows[i];
-    };
-
-
-    var columns = [
-    {
-      key: 'id',
-      name: 'ID'
-    },
-    {
-      key: 'title',
-      name: 'Title'
-    },
-    {
-      key: 'count',
-      name: 'Count'
-    }
-    ]
-
-        return  (<ReactDataGrid
-        columns={columns}
-        rowGetter={rowGetter}
-        rowsCount={_rows.length}
-        minHeight={500} />);
-
+// Table data as a list of array.
+const rows = [
+  ['a1', 'b1', 'c1'],
+  ['a2', 'b2', 'c2'],
+  ['a3', 'b3', 'c3'],
+  // .... and more
+];
+return (  <Table
+    rowHeight={50}
+    rowsCount={rows.length}
+    width={5000}
+    height={5000}
+    headerHeight={50}>
+    <Column
+      header={<Cell>Col 1</Cell>}
+      cell={<Cell>Column 1 static content</Cell>}
+      width={2000}
+    />
+    <Column
+      header={<Cell>Col 2</Cell>}
+      cell={<MyCustomCell mySpecialProp="column2" />}
+      width={1000}
+    />
+    <Column
+      header={<Cell>Col 3</Cell>}
+      cell={({rowIndex, ...props}) => (
+        <Cell {...props}>
+          Data for column 3: {rows[rowIndex][2]}
+        </Cell>
+      )}
+      width={2000}
+    />
+  </Table>);
 
   }
 });
